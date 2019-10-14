9c04f04b9f898dded01c6a829d7ec50f8010cac0
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2019-10-14 11:02:56 +1000

    plot 3 petal 🌹

--- build/build.js
+++ build/build.js
@@ -1,17 +1,40 @@
-var sketch = function (p) {
-    p.setup = function () {
-        p.createCanvas(p.windowWidth, p.windowHeight);
-    };
+var rose = function (p) {
     p.windowResized = function () {
         p.resizeCanvas(p.windowWidth, p.windowHeight);
     };
+    p.setup = function () {
+        p.createCanvas(p.windowWidth, p.windowHeight);
+        p.angleMode(p.DEGREES);
+    };
+    var n = 3;
+    var d = 29;
     p.draw = function () {
         p.background(0);
-        p.fill(200);
-        p.stroke(255);
-        p.strokeWeight(5);
-        p.rect(100, 100, p.width - 200, p.height - 200);
+        p.translate(p.width / 2, p.height / 2);
+        p.stroke(200);
+        p.strokeWeight(1);
+        p.noFill();
+        p.beginShape();
+        for (var i = 0; i < 360; i++) {
+            var k = i * d;
+            var r = p.sin(n * k) * 250;
+            var x = p.cos(k) * r;
+            var y = p.sin(k) * r;
+            p.vertex(x, y);
+        }
+        p.endShape(p.CLOSE);
+        p.stroke(200, 100, 122, 200);
+        p.strokeWeight(3);
+        p.beginShape();
+        for (var i = 0; i < 360; i++) {
+            var k = i;
+            var r = p.sin(n * k) * 250;
+            var x = p.cos(k) * r;
+            var y = p.sin(k) * r;
+            p.vertex(x, y);
+        }
+        p.endShape();
     };
 };
-new p5(sketch);
+new p5(rose);
 //# sourceMappingURL=build.js.map
--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,20 +1,52 @@
-const sketch = (p : p5) =>  {
-  p.setup = () => {
-    p.createCanvas(p.windowWidth, p.windowHeight)
-  }
+const rose = (p : p5) =>  {
 
   p.windowResized = () => {
     p.resizeCanvas(p.windowWidth, p.windowHeight)
   }
 
+
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight)
+    p.angleMode(p.DEGREES)
+  }
+
+  const n = 3
+  const d = 29
+
+
   p.draw = () => {
     p.background(0)
+    p.translate(p.width/2, p.height/2)
+    // inner lines
+    p.stroke(200)
+    p.strokeWeight(1)
+    p.noFill()
+    p.beginShape()
 
-    p.fill(200)
-    p.stroke(255)
-    p.strokeWeight(5)
-    p.rect(100, 100, p.width-200, p.height-200)
+    for (let i = 0; i < 360; i++) {
+      const k = i * d
+      const r = p.sin(n * k) * 250
+
+      const x = p.cos(k) * r
+      const y = p.sin(k) * r
+      p.vertex(x,y)
+    }
+    p.endShape(p.CLOSE)
+
+
+    // plot movement of the radius
+    p.stroke(200, 100, 122, 200)
+    p.strokeWeight(3)
+    p.beginShape()
+    for (let i = 0; i < 360; i++) {
+      const k = i // note i * d
+      const r = p.sin(n * k) * 250
+
+      const x = p.cos(k) * r
+      const y = p.sin(k) * r
+      p.vertex(x,y)
+    }
+    p.endShape()
   }
 }
-
-new p5(sketch)
+new p5(rose)
