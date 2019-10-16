fb0f3aa3f3660b2a933386c3cb2b79341e393f67
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2019-10-16 12:01:57 +1000

    boids: got cohesion working properly

--- build/build.js
+++ build/build.js
@@ -1,16 +1,103 @@
+var boids = 50;
+var nearBy = 80;
+var MaxForce = 0.95;
+var MaxSpeed = 7;
+var Boid = (function () {
+    function Boid(p) {
+        this.p = p;
+        this.pos = p.createVector(p.random(p.width), p.random(p.height));
+        this.velocity = p5.Vector.random2D();
+        this.velocity.setMag(p.random(2, 5));
+        this.accel = p.createVector();
+        this.speed = MaxSpeed;
+    }
+    Boid.prototype.draw = function () {
+        var _a = this, p = _a.p, pos = _a.pos, neighbours = _a.neighbours;
+        p.noStroke();
+        p.fill(130, 130, 210, 50 + 140 * 1 / (neighbours.length + 1));
+        p.ellipse(pos.x, pos.y, nearBy + 16, nearBy + 16);
+        p.fill(160);
+        p.ellipse(pos.x, pos.y, 16, 16);
+    };
+    Boid.prototype.align = function () {
+        var _a = this, neighbours = _a.neighbours, velocity = _a.velocity, speed = _a.speed, p = _a.p;
+        var steer = p.createVector();
+        if (neighbours.length == 0) {
+            return steer;
+        }
+        for (var _i = 0, neighbours_1 = neighbours; _i < neighbours_1.length; _i++) {
+            var b = neighbours_1[_i];
+            steer.add(b.velocity);
+        }
+        steer.div(neighbours.length);
+        steer.sub(velocity);
+        steer.setMag(speed);
+        steer.limit(MaxForce);
+        return steer;
+    };
+    Boid.prototype.cohesion = function () {
+        var _a = this, neighbours = _a.neighbours, pos = _a.pos, speed = _a.speed, velocity = _a.velocity, p = _a.p;
+        var steer = p.createVector();
+        if (neighbours.length == 0) {
+            return steer;
+        }
+        for (var _i = 0, neighbours_2 = neighbours; _i < neighbours_2.length; _i++) {
+            var b = neighbours_2[_i];
+            steer.add(b.pos);
+        }
+        steer.div(neighbours.length);
+        steer.sub(pos);
+        steer.setMag(speed);
+        steer.sub(velocity);
+        steer.limit(MaxForce);
+        return steer;
+    };
+    Boid.prototype.update = function (flock) {
+        var _this = this;
+        var _a = this, pos = _a.pos, velocity = _a.velocity, accel = _a.accel, p = _a.p;
+        this.neighbours = flock.filter(function (x) { return x != _this && pos.dist(x.pos) <= nearBy; });
+        var cohesion = this.cohesion();
+        this.accel = cohesion;
+        pos.add(velocity);
+        velocity.add(accel);
+        this.wrap();
+    };
+    Boid.prototype.wrap = function () {
+        var pos = this.pos;
+        if (pos.x < 0) {
+            pos.x = this.p.width;
+        }
+        if (pos.y < 0) {
+            pos.y = this.p.height;
+        }
+        if (pos.x > this.p.width) {
+            pos.x = 0;
+        }
+        if (pos.y > this.p.height) {
+            pos.y = 0;
+        }
+    };
+    return Boid;
+}());
 var sketch = function (p) {
+    var flock = [];
     p.setup = function () {
         p.createCanvas(p.windowWidth, p.windowHeight);
+        for (var i = 0; i < boids; i++) {
+            flock.push(new Boid(p));
+        }
     };
     p.windowResized = function () {
         p.resizeCanvas(p.windowWidth, p.windowHeight);
     };
     p.draw = function () {
         p.background(0);
-        p.fill(200);
-        p.stroke(255);
-        p.strokeWeight(5);
-        p.rect(100, 100, p.width - 200, p.height - 200);
+        p.fill(0);
+        for (var _i = 0, flock_1 = flock; _i < flock_1.length; _i++) {
+            var f = flock_1[_i];
+            f.update(flock);
+            f.draw();
+        }
     };
 };
 new p5(sketch);
--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,19 +1,144 @@
+const boids = 50
+
+const nearBy = 80
+const MaxForce =  0.95
+const MaxSpeed =  7
+
+class Boid {
+  p : p5
+  pos : p5.Vector
+  velocity : p5.Vector
+  accel : p5.Vector
+  speed : number
+  neighbours : Boid[]
+
+  constructor(p : p5) {
+    this.p = p
+    this.pos = p.createVector(p.random(p.width), p.random(p.height))
+
+    this.velocity = p5.Vector.random2D()
+    this.velocity.setMag(p.random(2, 5))
+    this.accel = p.createVector()
+
+    this.speed = MaxSpeed
+  }
+
+  draw() {
+    const {p, pos, neighbours} = this
+    p.noStroke()
+    p.fill(130, 130, 210, 50 + 140 * 1/(neighbours.length + 1))
+    p.ellipse(pos.x, pos.y, nearBy +16, nearBy +16)
+    p.fill(160)
+    p.ellipse(pos.x, pos.y, 16, 16)
+  }
+
+
+  align() {
+    const {neighbours,velocity, speed, p } = this
+    let steer = p.createVector()
+    if (neighbours.length == 0) {
+      return steer
+    }
+
+    for (let b of neighbours) {
+      steer.add(b.velocity)
+    }
+    steer.div(neighbours.length)
+
+    // find the steering vector
+    steer.sub(velocity)
+    steer.setMag(speed)
+    // ensure the turn is ma
+    steer.limit(MaxForce)
+    return steer
+  }
+
+  cohesion() {
+    const {neighbours, pos, speed, velocity, p} = this
+
+    let steer = p.createVector()
+    if (neighbours.length == 0) {
+      return steer
+    }
+
+    // avg position
+    for (let b of neighbours) {
+      steer.add(b.pos)
+    }
+    steer.div(neighbours.length)
+
+    // find the steering vector to that position
+    steer.sub(pos)
+    steer.setMag(speed)
+
+    // find the change in my velocity to reach there
+    steer.sub(velocity)
+    steer.limit(MaxForce)
+    return steer
+  }
+
+  update(flock : Boid[]) {
+    const {pos, velocity, accel, p} = this
+    this.neighbours = flock.filter(x => x != this && pos.dist(x.pos) <= nearBy)
+
+    //const steer = this.align()
+    //this.accel = steer
+
+    const cohesion = this.cohesion()
+    this.accel = cohesion
+
+    //steer.add(cohesion).limit(MaxForce)
+    //this.accel = steer
+
+    pos.add(velocity)
+    velocity.add(accel)
+    this.wrap()
+  }
+
+
+  wrap() {
+    let {pos} = this
+
+    if (pos.x < 0)  {
+      pos.x = this.p.width
+    }
+    if (pos.y < 0) {
+      pos.y = this.p.height
+    }
+
+    if (pos.x > this.p.width) {
+      pos.x = 0
+    }
+    if (pos.y > this.p.height) {
+      pos.y = 0
+    }
+  }
+
+}
+
 const sketch = (p : p5) =>  {
+  let flock : Boid[] = []
+
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight)
+    for (let i = 0; i < boids; i++) {
+      flock.push(new Boid(p))
+    }
   }
 
   p.windowResized = () => {
     p.resizeCanvas(p.windowWidth, p.windowHeight)
   }
 
+
+
   p.draw = () => {
     p.background(0)
-
-    p.fill(200)
-    p.stroke(255)
-    p.strokeWeight(5)
-    p.rect(100, 100, p.width-200, p.height-200)
+    p.fill(0)
+    for (let f of flock) {
+      f.update(flock)
+      f.draw()
+    }
   }
 }
 
