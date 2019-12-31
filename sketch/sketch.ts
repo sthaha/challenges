38df137de003dd3c8ba1dddf90bc055e17b3ce43
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2019-12-31 12:01:05 +1000

    Improvements based on the Coding train video
    Video: https://www.youtube.com/watch?v=--6eyLO78CY

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -3,46 +3,70 @@ enum Orientation {
   Horizontal,
 }
 
+type repeatFn = () => boolean
+
 class Plotter {
   p: p5
   h: Circle
   v: Circle
   points: p5.Vector[]
+  repeating: repeatFn
 
-  constructor(p: p5, h: Circle, v: Circle) {
+  color: p5.Color
+  dotColor: p5.Color
+
+  constructor(p: p5, h: Circle, v: Circle, repeating: repeatFn ) {
     this.p = p
     this.h = h
     this.v = v
+    this.repeating = repeating
 
     this.points = []
+
+    this.color = p.lerpColor(h.color, v.color, 0.5)
+    this.color.setAlpha(180)
+
+    this.dotColor = p.color(this.color.toString('rgba'))
+    this.dotColor.setAlpha(120)
+
   }
 
   draw() {
-    const {p, h, v} = this
+    const {p, h, v, points} = this
     const x  = h.knob.x
     const y  = v.knob.y
 
     const pt = p.createVector(x, y)
-    const store = this.points.length < 200 || this.points.length == 0 ||
-         pt.dist(this.points[this.points.length-1]) > 1
+
+    //console.log("point:", points)
+
+    const repeating = this.repeating()
+    const store = !repeating &&
+      (points.length == 0 || pt.dist(points[points.length -1]) > 1)
 
     if (store){
       this.points.push(pt)
     }
 
-    const clr = p.lerpColor(h.color, v.color, 0.3)
-    clr.setAlpha(120)
-    p.color(clr)
+    if (!repeating) {
+      p.stroke(this.color)
+    } else {
+      p.stroke(200, 200)
+    }
 
     p.noFill()
+    p.strokeWeight(2)
+
     p.beginShape()
-    for (const pt of this.points){
+    for (const pt of points){
       p.vertex(pt.x, pt.y)
     }
     p.endShape()
-    const dotColor = p.color(clr.toString('rgba'))
-    dotColor.setAlpha(100)
-    p.ellipse(pt.x, pt.y, 3,3)
+
+
+    p.stroke(this.dotColor)
+    p.fill(this.dotColor)
+    p.ellipse(pt.x, pt.y, 5,5)
   }
 
 }
@@ -62,15 +86,17 @@ class Circle {
 
   showLines: boolean = false
 
-  constructor(p: p5, o: Orientation,  x: number, y : number, radius: number, speed: number, color: p5.Color) {
+  constructor(p: p5, o: Orientation,  x: number, y : number, radius: number,
+              angle: number, speed: number,
+              color: p5.Color) {
     this.p = p
     this.orientation = o
     this.x = x
     this.y = y
     this.radius = radius
+    this.angle = angle
     this.speed = speed
     this.color = color
-    this.angle = 0
   }
 
   get knob(): p5.Vector {
@@ -91,7 +117,6 @@ class Circle {
 
     const knob = this.knob
     const knobColor =  p.color(color.toString('rgba'))
-    console.log(p.alpha(knobColor))
     knobColor.setAlpha(p.alpha(color) - 10)
 
     p.fill(knobColor)
@@ -103,9 +128,10 @@ class Circle {
     }
 
     const lineColor = p.color(color.toString('rgba'))
-    lineColor.setAlpha(p.alpha(color) - 20)
+    lineColor.setAlpha(p.alpha(color) - 80)
 
     p.strokeWeight(2)
+    p.stroke(lineColor)
 
     if (this.orientation == Orientation.Horizontal) {
       p.line(knob.x, 0, knob.x, p.height)
@@ -123,6 +149,7 @@ class Circle {
     this.angle += this.speed
   }
 }
+
 const sketch = (p : p5) =>  {
   let isLooping = true
   const noLoop = () => {isLooping = false; p.noLoop()}
@@ -139,52 +166,70 @@ const sketch = (p : p5) =>  {
         for (const c of circles) {
           c.toggleLines()
         }
+        p.redraw()
         break
     }
   }
 
 
-
-  const startX = 100
-  const startY = 100
   const radius = 50
-  const padding = 40
-  const speed = p.TWO_PI/(360*1.8)
+  const padding = 50
+  const speed = -p.TWO_PI/(360)
+  const startAngle = -p.HALF_PI
 
   const circles: Circle[] = []
-  const horizontal: Circle[] = []
-  const vertical: Circle[] = []
-
   const plotters: Plotter[] = []
 
 
-
-  for (let i =  1; i <= 5; i++) {
-    const x = startX + i * (radius * 2 + padding)
-
-    const hColor = p.color(120 + 25 * i, 225, 28, 200)
-    const h = new Circle(p, Orientation.Horizontal, x, startX, radius, speed*i, hColor)
-    horizontal.push(h)
-
-    const y = startY + i * (radius * 2 + padding)
-    const vColor = p.color(28, 225, 120 + 25 * i, 200)
-    const v = new Circle(p, Orientation.Vertical, startY, y, radius, speed*i, vColor)
-    v.angle = p.PI/2
-    vertical.push(v)
-    circles.push(h,v)
-  }
-
-  // horizontal
-  for (let i =  0; i < 5; i++) {
-    const h =  horizontal[i]
-    for (let j =  0; j < 5; j++) {
-      const v =  vertical[j]
-      plotters.push(new Plotter(p, h, v))
-    }
-  }
-
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight)
+
+    const d = radius * 2
+    const top = radius + padding/4
+    const hNum = (p.width - top) / (d+padding)
+    //const hNum = (p.width - padding*2)  / (d + padding)
+    const vNum = (p.height - padding*2) / (d + padding)
+    //const vNum = (p.height - padding*2) / (d + padding)
+
+
+    const horizontal: Circle[] = []
+    for (let i =  1; i <= hNum; i++) {
+      const x = top + i * (d + padding)
+
+      const hColor = p.color(120 + 25 * i, 225, 28, 200)
+      const h = new Circle(p, Orientation.Horizontal, x, top, radius,
+                           startAngle, speed*i, hColor)
+      horizontal.push(h)
+      circles.push(h)
+    }
+
+    const vertical: Circle[] = []
+    for (let i =  1; i <= vNum; i++) {
+      const y = top + i * (d + padding)
+      const vColor = p.color(28, 225, 120 + 25 * i, 200)
+      const v = new Circle(p, Orientation.Vertical, top, y, radius,
+                           startAngle, speed*i, vColor)
+      vertical.push(v)
+      circles.push(v)
+    }
+
+    // detect first loop
+    const rotation = startAngle + p.TWO_PI * Math.sign(speed)
+    const repeating = () => circles[0].angle < rotation
+
+    //const repeating = () => {
+      //const ret = circles[0].angle < rotation
+      //console.log(" ... angle", circles[0].angle,  "rotation", rotation,  "ret: ", ret)
+      //return ret
+    //}
+
+    // horizontal
+    for (const h of horizontal) {
+      for (const v of vertical) {
+        plotters.push(new Plotter(p, h, v, repeating))
+      }
+    }
+
     noLoop()
   }
 
