4bbf61d1b5e58501a615568e40baba8b2acf8bf6
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2020-01-15 18:04:25 +1000

    Beebot for 👧

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,5 +1,122 @@
 const sketch = (p : p5) =>  {
 
+//let instructions = "ffffrffflfffrfffrffflffflffffffffrfffrr";
+
+//const steps = instructions.split("");
+
+ const steps = [
+     'f', 'a', 'f', 'b', 'r',
+     //'f', 'f', 'f', 'l',
+     //'f', 'f','f', 'r',
+     //'f', 'f', 'f', 'r',
+     //'f', 'f', 'f', 'f', 'l',
+     //'f', 'f', 'f', 'r',
+     //'b', 'b', 'b', 'l', 'f', 'f', 'f', 'f', 'r',
+ ];
+
+
+
+
+const gridSize = 30;
+
+let beebot = {
+    x: 10,
+    y: 10,
+    angle: 0,
+};
+
+const drawBeebot = () => {
+
+  let x = beebot.x * gridSize + gridSize /2;
+  let y = beebot.y * gridSize + gridSize /2;
+
+  p.push();
+  p.translate(x,y);
+  p.stroke(255, 0, 0);
+  p.point(0, 0);
+
+  p.rotate(beebot.angle);
+  p.stroke(255, 0, 0);
+  p.fill(255, 0, 0);
+  p.ellipse(0, 0, 11, 11);
+
+  p.stroke(71, 17, 17);
+  p.fill(77, 9, 9);
+  p.ellipse(-4, -4, 4, 4);
+  p.ellipse(+4, -4, 4, 4);
+
+  p.pop();
+
+};
+
+const clearBeebot = () => {
+  const x = beebot.x * gridSize + 2;
+  const y = beebot.y * gridSize + 2;
+  const s = gridSize - 5;
+
+  p.stroke(255, 150);
+  p.fill(255, 150);
+  p.rect(x, y, s, s);
+
+};
+
+
+
+const move = (x: number, y: number, angle: number) => {
+  clearBeebot();
+  beebot.x = x;
+  beebot.y = y;
+  beebot.angle = angle;
+  drawBeebot();
+};
+
+const forward = () => {
+  clearBeebot();
+  switch (beebot.angle % 360) {
+      case 0: beebot.y--; break;
+      case 90: beebot.x++; break;
+      case 180: beebot.y++; break;
+      case 270: beebot.x--; break;
+  }
+
+  drawBeebot();
+};
+
+const back = () => {
+  clearBeebot();
+  switch (beebot.angle % 360) {
+    case 0: beebot.y++; break;
+    case 90: beebot.x--; break;
+    case 180: beebot.y--; break;
+    case 270: beebot.x++; break;
+  }
+
+  drawBeebot();
+};
+
+const turnRight = () => {
+  clearBeebot();
+  beebot.angle += 90;
+  drawBeebot();
+};
+
+const turnLeft = () => {
+  clearBeebot();
+  beebot.angle -= 90;
+  drawBeebot();
+};
+
+const abouturn  = () => {
+  clearBeebot();
+  beebot.angle += 180;
+  drawBeebot();
+};
+
+
+
+
+
+
   let isLooping = true
   const noLoop = () => {isLooping = false; p.noLoop()}
   const loop = () => {isLooping = true; p.loop()}
@@ -24,16 +141,41 @@ const sketch = (p : p5) =>  {
 
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight)
+    p.angleMode(p.DEGREES)
+    for (let i = 0; i< p.width; i+= gridSize) {
+        p.line(0, i, p.width, i);
+        p.line(i, 0, i, p.height);
+    }
+
+    move(0, 0, 90);
     noLoop()
   }
 
-  p.draw = () => {
-    p.background(0)
 
-    p.fill(200)
-    p.stroke(255)
-    p.strokeWeight(5)
-    p.rect(100, 100, p.width-200, p.height-200)
+  let current = 0;
+
+  p.draw = () => {
+    if (current >= steps.length) {
+      move(0, 0, 90);
+      current = 0;
+      return;
+    }
+
+    if (isLooping) {
+      const x = p.frameCount / 15 ;
+      if (x <= current+1) {
+        return;
+      }
+    }
+
+    switch (steps[current]) {
+        case 'f': forward(); break;
+        case 'r': turnRight(); break;
+        case 'l': turnLeft(); break;
+        case 'b': back(); break;
+        case 'a': abouturn(); break;
+    }
+    current++;
   }
 }
 
