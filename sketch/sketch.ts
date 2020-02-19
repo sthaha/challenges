ca42e23a6342d9710fa5bbf910e001f5e148cd72
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2020-02-19 21:26:15 +1000

    bouncing 🏀

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -10,11 +10,6 @@ const sketch = (p : p5) =>  {
       case p.ESCAPE: toggleLoop(); return;
       case 32: p.redraw(); return
     }
-    switch (p.key) {
-      case 'i':
-        p.redraw()
-        break
-    }
   }
 
 
@@ -24,16 +19,43 @@ const sketch = (p : p5) =>  {
 
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight)
-    noLoop()
+    //noLoop()
   }
 
-  p.draw = () => {
-    p.background(0)
+  let x = 120;
+  let y = 20;
+  let size = 80;
+  let dx = 2;
+  let dy = 2;
 
-    p.fill(200)
-    p.stroke(255)
-    p.strokeWeight(5)
-    p.rect(100, 100, p.width-200, p.height-200)
+
+  p.draw = () => {
+    p.background(180)
+    p.fill(20, 200)
+    p.noStroke()
+    p.circle(x, y, size)
+    p.stroke("red")
+    p.point(x,y)
+    dx = p.map(p.mouseX, 0, p.width, -3, 3)
+    dy = p.map(p.mouseY, 0, p.height, -3, 3)
+    //console.log("dx:", dx)
+    x += dx
+    y += dy
+
+    const r = size/2
+    let right = x + r;
+    let left = x - r;
+    if (right > p.width || left < 0) {
+      x = left < 0 ? r : p.width - r
+      dx = dx * -1  // 2 * -1 = -2  | -2 * -1 = 2
+    }
+
+    let top = y - r;
+    let bottom = y + r;
+    if (bottom > p.height || top < 0) {
+      y = top < 0 ? r : p.height - r
+      dy = dy * -1  // 2 * -1 = -2  | -2 * -1 = 2
+    }
   }
 }
 
