38f73a19fd04e30570737ad633f0ff42ba75bc76
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2020-02-23 15:16:18 +1000

    Create mandelbrot set ♥️

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -19,21 +19,65 @@ const sketch = (p : p5) =>  {
 
 
   p.windowResized = () => {
-    p.resizeCanvas(p.windowWidth, p.windowHeight)
+    const min = p.windowWidth < p.windowHeight ? p.windowWidth : p.windowHeight
+    p.resizeCanvas(min, min)
   }
 
   p.setup = () => {
-    p.createCanvas(p.windowWidth, p.windowHeight)
-    noLoop()
+    const min = p.windowWidth < p.windowHeight ? p.windowWidth : p.windowHeight
+    p.createCanvas(min, min)
+    //noLoop()
   }
 
+  const maxIterations = 80;
   p.draw = () => {
     p.background(0)
+    p.pixelDensity(1)
+    p.loadPixels()
 
-    p.fill(200)
-    p.stroke(255)
-    p.strokeWeight(5)
-    p.rect(100, 100, p.width-200, p.height-200)
+    let range = p.map(p.mouseX, 0, p.width, 0.50, 2)
+
+    for (let y = 0; y < p.height; y++) {
+      const row = y * p.width
+
+      for (let x = 0; x < p.width; x++){
+
+        // a + bi
+        const a = p.map(x, 0, p.width, -range, range)
+        const b = p.map(y, 0, p.height, -range, range)
+
+        let real = a
+        let img  = b
+        let growth = 0
+
+        for (let i = 0; i < maxIterations; i++ ) {
+          // a2 - b2 + 2abi
+          // r = a2 = b2
+          // i = 2abi
+
+          const r =  real * real - img * img
+          const i =  2 * real * img
+
+          real = r + a
+          img = i + b
+
+          if (real*real + img*img > 30) {
+            break
+          }
+          growth++
+        }
+
+        const brightness = p.map(growth, 0, maxIterations, 0, 255)
+
+
+        const pix = (row + x) * 4
+        p.pixels[pix + 0] = brightness // rbga
+        p.pixels[pix + 1] = brightness
+        p.pixels[pix + 2] = brightness
+        p.pixels[pix + 3] = 200
+      }
+    }
+    p.updatePixels()
   }
 }
 
