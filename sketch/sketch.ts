edfc9afe289a90567c7f08944963286d2e436493
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2020-03-06 21:59:29 +1000

    fractal using just lines

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -27,13 +27,39 @@ const sketch = (p : p5) =>  {
     noLoop()
   }
 
+
   p.draw = () => {
     p.background(0)
+    p.translate(p.width/2, p.height/2)
+    p.stroke(222, 16, 16, 170)
+    p.strokeWeight(2)
+    p.fill(170, 160)
 
-    p.fill(200)
-    p.stroke(255)
-    p.strokeWeight(5)
-    p.rect(100, 100, p.width-200, p.height-200)
+    const scale = 1.058
+    let len = 100
+    let p0 = p.createVector()
+    let p1 = p.createVector(len, 0)
+    p.line(p0.x, p0.y, p1.x, p1.y)
+
+    let angle = p.PI/2 + p.PI/8
+    //len *= scale
+    const x = len * p.cos(angle) + p1.x
+    const y = len * p.sin(angle) + p1.y
+    let c = p.createVector(x, y)
+    p.line(p1.x, p1.y, c.x, c.y)
+
+
+    const N = 68
+    for (let i = 0; i< N; i++){
+      let v = p0.sub(c)
+      v.mult(scale)
+      const next = p5.Vector.add(c, v)
+      p.line(c.x, c.y, next.x, next.y)
+      p0 = p1
+      p1 = c
+      c = next
+    }
+
   }
 }
 
