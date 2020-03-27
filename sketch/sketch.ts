2778e3f860c40ee46dd6b3eb3493c60e8b917f8d
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2020-03-27 20:20:58 +1000

    simulate virus spread

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,3 +1,109 @@
+const InfectionRadius = 25
+const Movement = 3.5
+const N = 100;
+const diameter = 6
+const Recovery = 20
+const RecoveryRate = 0.05
+
+class Person {
+  p: p5
+  population: Person[]
+
+  pos: p5.Vector
+  v: p5.Vector
+  infected = false
+  recovery: number = 0
+
+  constructor(p: p5, pos: p5.Vector, population: Person[]){
+    this.p = p
+    this.pos = pos
+    this.v = p.createVector(p.random(-Movement, Movement), p.random(-Movement, Movement))
+    this.population = population
+  }
+
+  infect(){
+    if (this.infected){
+      return
+    }
+
+    this.infected = true
+    this.recovery = Recovery
+  }
+
+  recovered() {
+    if (!this.infected){
+      return true
+    }
+
+    this.recovery -= RecoveryRate
+    this.infected = this.recovery >= 0
+    return !this.infected
+  }
+
+  update(){
+    const {p, pos, v, infected, population} = this
+
+    this.pos.add(v)
+    const r = p.createVector(p.random(-0.8, 0.8), p.random(-0.8, 0.8))
+    this.pos.add(r)
+
+    this.wrap()
+
+    if (this.recovered()){
+      return
+    }
+
+    for (const other of population) {
+      if (other == this){
+        continue
+      }
+      if (this.pos.dist(other.pos) < InfectionRadius) {
+        other.infect()
+      }
+    }
+  }
+
+  draw() {
+    const {p, pos, infected} = this
+
+    if (infected) {
+      p.fill(250,10, 120, 80)
+    } else {
+      p.fill(25,210, 120, 20)
+    }
+    p.circle(pos.x, pos.y, InfectionRadius)
+
+    if (infected) {
+      p.fill(250,10, 120)
+    } else {
+      p.fill(70,120, 180)
+    }
+    p.circle(pos.x, pos.y, diameter)
+  }
+
+  wrap() {
+    const {p, pos} = this
+    const {x, y} = pos
+    const {width, height} = p
+
+    const d = diameter
+    const r = d/2
+
+    if (x-d  > width){
+      this.pos.x = -r
+    }else if (x+d < 0){
+      this.pos.x = width+r
+    }
+
+    if (y-d > height){
+      this.pos.y = -r
+    }else if (y+d < 0){
+      this.pos.y = height+r
+    }
+
+  }
+
+}
 const sketch = (p : p5) =>  {
 
   let isLooping = true
@@ -10,11 +116,6 @@ const sketch = (p : p5) =>  {
       case p.ESCAPE: toggleLoop(); return;
       case 32: p.redraw(); return
     }
-    switch (p.key) {
-      case 'i':
-        p.redraw()
-        break
-    }
   }
 
 
@@ -22,18 +123,32 @@ const sketch = (p : p5) =>  {
     p.resizeCanvas(p.windowWidth, p.windowHeight)
   }
 
+  const population: Person[] = []
+
+  const randV = (x: number, y: number) => p.createVector(p.random(x), p.random(y))
+
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight)
+    for(let i = 0; i < N; i++) {
+      population.push(new Person(p, randV(p.width, p.height), population))
+    }
+    population[Math.trunc(p.random(N))].infect()
+
+    p.noStroke()
     noLoop()
+
   }
 
   p.draw = () => {
-    p.background(0)
+    if (population.filter((x) => x.infected == false).length == 0) {
+      noLoop()
+    }
 
-    p.fill(200)
-    p.stroke(255)
-    p.strokeWeight(5)
-    p.rect(100, 100, p.width-200, p.height-200)
+    p.background(17)
+    for (const person of population) {
+      person.update()
+      person.draw()
+    }
   }
 }
 
