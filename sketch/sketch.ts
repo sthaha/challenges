2818dd7f70ddbfba08137a7b7bba01c48e33f93a
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2020-03-27 22:35:11 +1000

    simulate death and recovery

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,9 +1,16 @@
-const InfectionRadius = 25
-const Movement = 3.5
-const N = 100;
-const diameter = 6
+const InfectionRadius = 20
+const Movement = 1.5
+const N = 200;
+const diameter = 5
 const Recovery = 20
 const RecoveryRate = 0.05
+
+enum Stage {
+  Uninfected,
+  Infected,
+  Recovered,
+  Terminated,
+}
 
 class Person {
   p: p5
@@ -11,37 +18,51 @@ class Person {
 
   pos: p5.Vector
   v: p5.Vector
-  infected = false
-  recovery: number = 0
+
+  stage = Stage.Uninfected
+  infection: number = 0
+  health: number
+
 
   constructor(p: p5, pos: p5.Vector, population: Person[]){
     this.p = p
     this.pos = pos
     this.v = p.createVector(p.random(-Movement, Movement), p.random(-Movement, Movement))
     this.population = population
+    this.health = p.random(5, 20)
   }
 
   infect(){
-    if (this.infected){
+    if (this.stage == Stage.Uninfected  ){
+      this.stage = Stage.Infected
+      this.infection = Recovery
+    }
+  }
+
+  recover() {
+    const {stage} = this
+    if (stage != Stage.Infected ){
       return
     }
 
-    this.infected = true
-    this.recovery = Recovery
-  }
-
-  recovered() {
-    if (!this.infected){
-      return true
+    this.health -= 0.023
+    if (this.health <= 0.0) {
+      this.stage = Stage.Terminated
+      return
     }
 
-    this.recovery -= RecoveryRate
-    this.infected = this.recovery >= 0
-    return !this.infected
+    this.infection -= RecoveryRate
+    if (this.infection <= 0) {
+      this.stage = Stage.Recovered
+    }
   }
 
   update(){
-    const {p, pos, v, infected, population} = this
+    const {p, pos, v, population, stage} = this
+
+    if (stage == Stage.Terminated){
+      return
+    }
 
     this.pos.add(v)
     const r = p.createVector(p.random(-0.8, 0.8), p.random(-0.8, 0.8))
@@ -49,9 +70,10 @@ class Person {
 
     this.wrap()
 
-    if (this.recovered()){
+    if (this.stage != Stage.Infected){
       return
     }
+    this.recover()
 
     for (const other of population) {
       if (other == this){
@@ -64,19 +86,22 @@ class Person {
   }
 
   draw() {
-    const {p, pos, infected} = this
 
-    if (infected) {
-      p.fill(250,10, 120, 80)
-    } else {
-      p.fill(25,210, 120, 20)
+    const {p, pos, stage} = this
+
+    switch (stage) {
+    case Stage.Uninfected: p.fill(50,180, 220, 100); break;
+    case Stage.Infected: p.fill(250,10, 120, 80); break;
+    case Stage.Recovered: p.fill(150,120, 220, 180); break;
+    case Stage.Terminated: p.fill(110,110, 110, 80); break;
     }
     p.circle(pos.x, pos.y, InfectionRadius)
 
-    if (infected) {
-      p.fill(250,10, 120)
-    } else {
-      p.fill(70,120, 180)
+    switch (stage) {
+    case Stage.Uninfected: p.fill(70,120, 180); break;
+    case Stage.Infected: p.fill(250,10, 120); break;
+    case Stage.Recovered: p.fill(150,120, 220); break;
+    case Stage.Terminated: p.fill(130,130, 130); break;
     }
     p.circle(pos.x, pos.y, diameter)
   }
@@ -140,7 +165,9 @@ const sketch = (p : p5) =>  {
   }
 
   p.draw = () => {
-    if (population.filter((x) => x.infected == false).length == 0) {
+    if (population.filter((x) => x.stage == Stage.Infected).length == 0) {
+      const fatality = population.reduce((a, x) => x.stage == Stage.Terminated ? a+1 : a, 0)
+      console.log("Fatalities: ", fatality)
       noLoop()
     }
 
