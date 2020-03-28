30753d1b94dbb0f6fef479bf3a1c650956959a1b
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2020-03-28 22:29:47 +1000

    graph it

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,7 +1,8 @@
-const InfectionRadius = 20
-const Movement = 1.5
-const N = 200;
-const diameter = 5
+const InfectionRadius = 25
+const Movement = 1.2
+const N = 100;
+
+const PersonSize = 5
 const Recovery = 20
 const RecoveryRate = 0.05
 
@@ -65,7 +66,10 @@ class Person {
     }
 
     this.pos.add(v)
-    const r = p.createVector(p.random(-0.8, 0.8), p.random(-0.8, 0.8))
+    const range = 1.2
+    const r = p.createVector(
+      p.random(-range, range),
+      p.random(-range, range))
     this.pos.add(r)
 
     this.wrap()
@@ -90,9 +94,9 @@ class Person {
     const {p, pos, stage} = this
 
     switch (stage) {
-    case Stage.Uninfected: p.fill(50,180, 220, 100); break;
+    case Stage.Uninfected: p.fill(50,180, 220, 80); break;
     case Stage.Infected: p.fill(250,10, 120, 80); break;
-    case Stage.Recovered: p.fill(150,120, 220, 180); break;
+    case Stage.Recovered: p.fill(150,120, 220, 80); break;
     case Stage.Terminated: p.fill(110,110, 110, 80); break;
     }
     p.circle(pos.x, pos.y, InfectionRadius)
@@ -101,9 +105,9 @@ class Person {
     case Stage.Uninfected: p.fill(70,120, 180); break;
     case Stage.Infected: p.fill(250,10, 120); break;
     case Stage.Recovered: p.fill(150,120, 220); break;
-    case Stage.Terminated: p.fill(130,130, 130); break;
+    case Stage.Terminated: p.fill(230,130, 130); break;
     }
-    p.circle(pos.x, pos.y, diameter)
+    p.circle(pos.x, pos.y, PersonSize)
   }
 
   wrap() {
@@ -111,7 +115,7 @@ class Person {
     const {x, y} = pos
     const {width, height} = p
 
-    const d = diameter
+    const d = PersonSize
     const r = d/2
 
     if (x-d  > width){
@@ -164,18 +168,82 @@ const sketch = (p : p5) =>  {
 
   }
 
+  const stageCount = (s: Stage ) => population.reduce((a, x) => x.stage == s ? a+1: a, 0)
+
+  const infected: number[] = []
+  const recovered: number[] = []
+  const fatal:  number[]= []
+  const uninfected:  number[]= []
+
+  const stats = () => {
+
+    const i = stageCount(Stage.Infected)
+    const f = stageCount(Stage.Terminated)
+    const r = stageCount(Stage.Recovered)
+    const u = population.length - (i + r + f)
+
+
+    infected.push(i)
+    fatal.push(f)
+    recovered.push(r)
+    uninfected.push(u)
+
+    const border = 2
+    const w = p.width - border* 2
+    //const w = population.length * 2.5
+    const h = population.length * 1.15
+
+    const x = 0 // p.width - w -2
+    //const x = (p.width - w - border) /2
+    const y = p.height - h - border
+
+    p.strokeWeight(border)
+    p.stroke(113, 113, 113, 200)
+    p.fill(13, 13, 13, 178)
+
+    p.rect(x, y, w, h)
+
+    const graph = (data : number[]) => {
+      p.beginShape()
+      const start = x + border
+      const base = y + h - border
+
+      const l = data.length
+      const trim = p.max(0, l - (w - border * 2))
+      for (let i = 0;  i < l; i++) {
+        p.vertex(start+i, base - data[i+trim])
+      }
+      p.endShape()
+      p.circle(start -trim + l-1, base - data[l - 1], 3)
+    }
+
+    p.stroke(78, 148, 178)
+    graph(uninfected)
+
+    p.stroke(218, 88, 18)
+    graph(infected)
+
+    p.stroke(240, 18, 18)
+    graph(fatal)
+
+    p.stroke(150,120, 220)
+    graph(recovered)
+  }
+
   p.draw = () => {
-    if (population.filter((x) => x.stage == Stage.Infected).length == 0) {
-      const fatality = population.reduce((a, x) => x.stage == Stage.Terminated ? a+1 : a, 0)
-      console.log("Fatalities: ", fatality)
+    const infections = stageCount(Stage.Infected)
+    if ( infections == 0 ) {
+      stats()
       noLoop()
     }
 
     p.background(17)
+    p.noStroke()
     for (const person of population) {
       person.update()
       person.draw()
     }
+    stats()
   }
 }
 
