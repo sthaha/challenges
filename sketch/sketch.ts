8256042564f265b4dd04740cf7e607789ee10108
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2021-01-16 00:08:32 +1000

    implement cardiod

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -10,30 +10,62 @@ const sketch = (p : p5) =>  {
       case p.ESCAPE: toggleLoop(); return;
       case 32: p.redraw(); return
     }
-    switch (p.key) {
-      case 'i':
-        p.redraw()
-        break
-    }
   }
 
 
-  p.windowResized = () => {
-    p.resizeCanvas(p.windowWidth, p.windowHeight)
-  }
+  p.windowResized = () => { p.resizeCanvas(p.windowWidth, p.windowHeight) }
 
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight)
     noLoop()
   }
 
-  p.draw = () => {
-    p.background(0)
+  interface point {
+    x: number
+    y: number
+  }
 
-    p.fill(200)
-    p.stroke(255)
-    p.strokeWeight(5)
-    p.rect(100, 100, p.width-200, p.height-200)
+
+  const computePoints = (r: number, n: number) => {
+    const points: point[] = []
+    for (let i = 0; i<n; i++) {
+      const x = p.cos(i/n *p.TWO_PI) * r;
+      const y = p.sin(i/n *p.TWO_PI) * r;
+      points.push({x,y})
+    }
+    return points;
+  }
+
+
+  const radius = 300;
+  const slices = 99;
+
+  let mult = 0.12;
+  p.draw = () => {
+    p.translate(p.width/2, p.height/2)
+    p.background(0)
+    p.noFill()
+    p.stroke(128, 100, 100)
+    p.strokeWeight(4)
+    // p.circle(0, 0, radius*2)
+    // p.point(0,0)
+
+    p.stroke(18, 180, 210)
+
+    const points = computePoints(radius, slices)
+    for (const pt of points) {
+      p.circle(pt.x, pt.y, 4)
+    }
+
+    p.strokeWeight(2)
+
+    for (let i=1; i<slices; i++) {
+      const p1 = points[i]
+      const x = p.cos(i * mult/slices * p.TWO_PI) * radius;
+      const y = p.sin(i * mult/slices * p.TWO_PI) * radius;
+      p.line(p1.x, p1.y, x, y)
+    }
+    mult += 0.0034
   }
 }
 
